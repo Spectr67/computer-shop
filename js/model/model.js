a03b37253e75cf075ddb8e4c5e6cb96177f175dc
YouROCK <yourock88@meta.ua>
2025-01-19 00:10:07 +0200

    add comments to model.js file

--- js/model/model.js
+++ js/model/model.js
@@ -3,27 +3,14 @@ const model = {
   favorite: [],
   cart: [],
   comparison: [],
-  query: '',
-  sortingType: 'ByPriceASC',
 
   searchProducts() {
     if (this.query === '') return this.products
-    return search.findProductsByQuery(this.query, this.products)
-  },
-
-  sortProductsAToZ() {
-    return this.products.sort((a, b) => a.caption.localeCompare(b.caption))
-  },
-
-  sortProductsZToA() {
-    return this.products.sort((a, b) => b.caption.localeCompare(a.caption))
-  },
-
-  sortProductsByPriceHigh() {
-    return this.products.sort((a, b) => a.price - b.price)
-  },
-
-  sortProductsByPriceLow() {
-    return this.products.sort((a, b) => b.price - a.price)
+    return searcher.search(this.query, this.products)
   },
 }
+
+// push pop shift unshift sort reverse splice
+
+// чистая функция, которая не читает из глобального состояни
+// и не пишет в него
